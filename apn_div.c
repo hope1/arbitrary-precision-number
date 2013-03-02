b5802e7d1c0bb3b864f521b771ebe04276970557
hope1 <jeremyxie357@gmail.com>
2013-03-03 01:44:10 +0800

    remove testing code

--- apn_div.c
+++ apn_div.c
@@ -51,7 +51,7 @@ void apn_div(apn_s* dest_quot, apn_s* dest_rem, const apn_s* op1, const apn_s* o
         apn_assign_dig(dest_quot, 0); 
         return;
     }
-    apn_div_bz(dest_quot, dest_rem, op1, op2);
+    apn_div_basecase(dest_quot, dest_rem, op1, op2);
 }
 
 // long division
@@ -84,67 +84,5 @@ void apn_div_basecase(apn_s* dest_quot, apn_s* dest_rem, const apn_s* op1, const
     if(dest_rem != NULL)
         apn_swap(&rem, dest_rem);
     apn_clear_list(&quot, &rem, NULL);
-}
-
-// Burnikel-Ziegler divison, see
-// Christoph Burnikel and Joachim Ziegler, "Fast Recursive Division", October 1998
-
-void apn_div_bz(apn_s* dest_quot, apn_s* dest_rem, const apn_s* op1, const apn_s* op2) {
-//    if(op1->_size <= APN_DIV_BZ_THRESHOLD) {
-//        apn_div_basecase(dest_quot, dest_rem, op1, op2);
-//        return;
-//    }
-    // r-digit divide by s-digit number
-    apn_s A, B, Z, Q, R, V; // A, B: copy of operands, Q, R: results, Z, V: temp
-    apn_init_list(&A, &B, &Z, &Q, &R, &V, NULL);
-    apn_assign(&A, op1);
-    apn_assign(&B, op2);
-    // min{2^k | 2^k * BLOCKSIZE > s}, number of blocks to be divided
-    size_t m = (B._size / APN_DIV_BZ_BLOCKSIZE);
-    size_t t = !(!m || m & (m - 1)); // is power of 2?
-    // m = next power of 2
-    while(m >>= 1)
-        ++t;
-    m = 1 << t;
-    // n = ceil(s/m) * m
-    size_t n = (B._size / m + (bool)(B._size % m)) * m;
-    // extend and normalize B, shift the same amount for A
-    apn_shl(&B, &B, n - B._size);
-    apn_shl(&A, &A, n - B._size);
-    size_t sigma = AP_DIG_BIT - ap_dig_msb(B._data[B._size - 1]) - 1;
-    apn_bit_shl(&B, &B, sigma);
-    apn_bit_shl(&A, &A, sigma);
-    // find t = min {l >= 2 | A < base^(l*n) / 2}
-    apn_assign_dig(&V, 1);
-    apn_bit_shl(&V, &V, AP_DIG_BIT - 1); // base / 2
-    apn_shl(&V, &V, 2 * n - 1);
-    t = 2;
-    while(apn_cmp(&A, &V) >= 0) {
-        apn_shl(&V, &V, n);
-        ++t;
-    }
-    // main loop
-    // Z[t-2] = (A[t-1], A[t-2])
-    apn_assign_part_zero(&Z, &A, (t - 2) * n, 2 * n);
-    size_t i = t - 2;
-    do {
-        apn_div_basecase(&V, &R, &Z, &B); // TESTING
-//        apn_div_bz_d2n1n(&V, &R, &Z, &B);
-        apn_shl(&Q, &Q, n);
-        apn_add(&Q, &Q, &V);
-        if(i != 0) { // set Z = (R, A[i - 1])
-            apn_assign(&Z, &R);
-            apn_shl(&Z, &Z, n);
-            apn_assign_part_zero(&V, &A, (i - 1) * n, n);
-            apn_add(&Z, &Z, &V);
-        }
-    } while(i-- != 0);
-    // reduce remainder
-    apn_shr(&R, &R, n - B._size);
-    apn_bit_shr(&R, &R, sigma);
-
-    apn_swap(dest_quot, &Q);
-    apn_swap(dest_rem, &R);
-    apn_clear_list(&A, &B, &Z, &Q, &R, &V, NULL);
 }
 
